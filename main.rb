27d4f8df982b6eeec8fec6c3e3e18363697bcc55
rustam023 <r.91@mail.ru>
2020-11-05 12:48:57 +0300

    add jokers

--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 suits = %w[♦ ♥ ♣ ♠]
 
 cards = []
-values.each { |value| suits.each { |suit| cards << "#{value}#{suit}" } }
+values.each { |value| suits.each { |suit| cards << "#{value} of #{suit}" } }
+
+2.times {cards << '[Jocker]'}
 
 cards.shuffle!
 puts "number of cards to pick?"
