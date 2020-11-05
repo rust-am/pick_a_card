1e2f212a1fe289490b3dd3892028892fee4dc772
rustam023 <r.91@mail.ru>
2020-11-05 12:12:22 +0300

    add multiple cards

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
 suits = %w[♦ ♥ ♣ ♠]
-puts "#{values.sample}#{suits.sample}"
 
+cards = []
+values.each { |value| suits.each { |suit| cards << "#{value}#{suit}" } }
+
+cards.shuffle!
+puts "number of cards to pick?"
+number = STDIN.gets.to_i
+
+number.times { puts cards.pop }
